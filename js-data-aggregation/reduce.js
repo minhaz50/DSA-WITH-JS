2612bf47ede2766c09e1b4de7b61d68e89c02922
Md Minhaz Uddin <md.minhazuddin2597@gmail.com>
2026-04-23 07:04:26 +0600

    More reduce example added to the file

--- js-data-aggregation/reduce.js
+++ js-data-aggregation/reduce.js
@@ -10,3 +10,31 @@ const total = numbers1.reduce((acc, curr) => acc + curr, 0);
 
 console.log("Total is:", sum);
 console.log("Total is:", total);
+
+// Find Maximum Number
+
+const nums = [4, 6, 2, 62, 23, 65];
+
+const maxNums = nums.reduce((acc, curr) => {
+  return curr > acc ? curr : acc;
+}, nums[0]);
+
+console.log("The maximum number is: ", maxNums);
+
+// Count items
+
+const fruits = ["apple", "banana", "apple", "banana", "Watermealon"];
+
+const count = fruits.reduce((acc, fruit) => {
+  acc[fruit] = (acc[fruit] || 0) + 1;
+  return acc;
+}, {});
+console.log(count);
+
+// Flatten Array
+
+const arr = [[1, 2], [3, 4], [5]];
+
+const flat = arr.reduce((acc, curr) => acc.concat(curr), []);
+
+console.log(flat);
