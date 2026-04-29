9f281c2274fb71d6e6723461f7d18c2c12e51181
Md Minhaz Uddin <md.minhazuddin2597@gmail.com>
2026-04-29 13:57:53 +0600

    Learning Js OOP. How to create instance of Object.

---
+++ js-oop/instance-obj.js
@@ -0,0 +1,59 @@
+// Here are all ways to create instances of Object in JavaScript.
+// Object Literal (most common)
+
+/*
+let person = {
+  first_name: "Minhaz", // Here first_name = key and last_name = value
+  last_name: "Uddin",
+  age: 28,
+  city: "Mymensingh",
+
+  getName: () => {
+    return `My full Name is ${this.first_name} ${person.last_name}`;
+  },
+
+  greet() {
+    console.log("Hello" + this.first_name);
+  },
+};
+
+// console.log("My first name is: ", person.first_name);
+// console.log("My last name is: ", person.last_name);
+// console.log(person.getName());
+
+// person.getName();
+// person.greet();
+
+*/
+
+// Creating instance of Object Constructor (new Object())
+
+let person = new Object();
+
+((person.first_name = "Minhaz"),
+  (person.last_name = "Uddin"),
+  (person.age = 29),
+  (person.city = "Bhaluka"),
+  (person.isBangladeshi = true));
+
+person.getName = () => {
+  return `My full name is : ${person.first_name} ${person.last_name}`;
+};
+
+// console.log(person.getName());
+
+// Constructor Function
+function User(name, age, city) {
+  this.name = name;
+  this.age = age;
+  this.city = city;
+  this.greet = function () {
+    console.log(
+      `Hello, I'm ${this.name}, my age is ${this.age}.I am from ${this.city}`,
+    );
+  };
+}
+
+const user1 = new User("Minhaz", 25, "Mymensingh");
+
+user1.greet();
